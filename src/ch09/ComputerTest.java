ee1d6b9353205ad1ec99eb758a289bcd812c06bd
Gwak seyeon <ksyk1205@naver.com>
2021-06-28 12:24:44 +0900

    day04 - 인터페이스

---
+++ src/ch09/ComputerTest.java
@@ -0,0 +1,15 @@
+package ch09;
+
+public class ComputerTest {
+
+	public static void main(String[] args) {
+		
+		Computer desktop = new DeskTop();
+		desktop.display();
+		desktop.turnOff();
+		
+		NoteBook myNote = new MyNoteBook();
+
+	}
+
+}
---
+++ src/ch11/CalculatorTest.java
@@ -0,0 +1,21 @@
+package ch11;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 2;
+		
+		Calc calc = new CompleteCalc();
+		
+		System.out.println(num1 + "+" + num2 + "=" + calc.add(num1, num2));
+		System.out.println(num1 + "-" + num2 + "=" +calc.substract(num1, num2));
+		System.out.println(num1 + "*" + num2 + "=" +calc.times(num1, num2));
+		System.out.println(num1 + "/" + num2 + "=" +calc.divide(num1, num2));
+
+		
+		
+	}
+
+}
